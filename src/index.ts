fd668824dbc57cc98cd50b0136ed2c346518ea4d
Cihaan <cihankafadar1@gmail.com>
2024-07-11 10:46:01 +0200

    up

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,20 @@
-import {Context, Hono} from "hono";
+import { serve } from "@hono/node-server";
+import { Context, Hono } from "hono";
+import { cors } from "hono/cors";
 import * as _jsonwebtoken from "jsonwebtoken";
-import {sign as signType, verify as verifyType} from "jsonwebtoken";
-import {serve} from "@hono/node-server";
-import {cors} from "hono/cors";
-import {createDatabaseConnection} from "./database";
-import {appConfiguration} from "./env/env";
+import { sign as signType, verify as verifyType } from "jsonwebtoken";
+import { WebSocketServer } from "ws";
+import { createDatabaseConnection } from "./database";
+import { appConfiguration } from "./env/env";
 import roleBasedMiddleware from "./middleware/middleware";
+import { Message } from "./models/message";
 import announcements from "./routes/announcement";
 import auth from "./routes/auth";
 import categories from "./routes/categorie";
+import messages from "./routes/message";
 import reviews from "./routes/review";
 import skills from "./routes/skill";
 import users from "./routes/user";
-import {WebSocketServer} from 'ws';
-import messages from "./routes/message";
-import {Message} from "./models/message";
-import {Types} from "mongoose";
 
 const jsonwebtoken = <any>_jsonwebtoken;
 
@@ -29,20 +28,16 @@ const app = new Hono();
 
 // CORS middleware
 app.use(
-    "*",
-    cors({
-        origin: "http://localhost:3000",
-        allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-        allowHeaders: ["*"],
-        exposeHeaders: ["Content-Length", "X-Kuma-Revision"],
-        maxAge: 600,
-        credentials: true,
-    })
+  "*",
+  cors({
+    origin: "http://localhost:3000",
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept", "Origin", "Access-Control-Allow-Headers"],
+    exposeHeaders: ["Content-Length", "X-Kuma-Revision"],
+    maxAge: 600,
+    credentials: true,
+  })
 );
-
-// AUTH MIDDLWARE TODO: handle routes and methods
-// app.use(BASE_ROUTE + "/*", authMiddleware);
-// app.use(BASE_ROUTE + "/categories", authMiddleware);
 
 await createDatabaseConnection();
 
@@ -59,46 +54,46 @@ app.route(BASE_ROUTE, messages);
 app.route("/", auth);
 
 app.get("/api/healthz", (c: Context) => {
-    return c.text("OK");
+  return c.text("OK");
 });
 
 // Error handling middleware
 app.onError((err, c) => {
-    console.error(`${err}`);
-    return c.text("Internal Server Error", 500);
+  console.error(`${err}`);
+  return c.text("Internal Server Error", 500);
 });
 
 // Not Found handler
 app.notFound((c) => {
-    return c.text("Not Found", 404);
+  return c.text("Not Found", 404);
 });
 
 // Serve HTTP with WebSocket support on port PORT
 const server = serve({
-    fetch: app.fetch,
-    port: PORT,
+  fetch: app.fetch,
+  port: PORT,
 });
 
 // @ts-ignore
-const wss = new WebSocketServer({server});
+const wss = new WebSocketServer({ server });
 
 wss.on("connection", function connection(ws: any) {
-    ws.on("error", console.error);
-    ws.on("message", async function message(data: any) {
-        //conver data buffer to object
-        let dataSave = JSON.parse(data);
-        const newMessage = new Message({
-            user: dataSave.user,
-            announcement:dataSave.announcement,
-            sender: dataSave.sender,
-            content: dataSave.content
-        });
-
-        await newMessage.save();
-
-        // Broadcast the message to all clients
-            if (ws.readyState === ws.OPEN) {
-                ws.send(data);
-            }
+  ws.on("error", console.error);
+  ws.on("message", async function message(data: any) {
+    //conver data buffer to object
+    let dataSave = JSON.parse(data);
+    const newMessage = new Message({
+      user: dataSave.user,
+      announcement: dataSave.announcement,
+      sender: dataSave.sender,
+      content: dataSave.content,
     });
+
+    await newMessage.save();
+
+    // Broadcast the message to all clients
+    if (ws.readyState === ws.OPEN) {
+      ws.send(data);
+    }
+  });
 });
